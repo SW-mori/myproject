35a2b5a81440162247c8d56639f0e8e344c6c364
Tatsu Mori <tmori@skywill.jp>
2022-07-04 19:14:17 +0900

    js修正

--- src/js/common/media.js
+++ src/js/common/media.js
@@ -41,9 +41,9 @@ $(function() {
 
   $(window).on('scroll', function() {
     $('.scroll').each(function() {
-      let scroll = $(window).scrollTop();
-      let target = $(this).offset().top;
-      let windowHeight = $(window).height();
+      const scroll = $(window).scrollTop();
+      const target = $(this).offset().top;
+      const windowHeight = $(window).height();
       if (scroll > target - windowHeight + 200) {
         $(this).css('opacity','1');
         $(this).css('transform','translateY(0)');
